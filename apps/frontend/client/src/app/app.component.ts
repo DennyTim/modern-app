7d963dbb01e7ad82e5c9e523c9ee04a3940a2fae
dety0819 <luckystudydanit@gmail.com>
2021-04-11 17:57:06 +0300

    initial commit

---
+++ apps/frontend/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'modern-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend-client';
+}
