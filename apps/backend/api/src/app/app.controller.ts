b8b04a743d8cf89154ca8a742591a3fa284d2b01
dety0819 <luckystudydanit@gmail.com>
2021-04-25 16:32:47 +0300

    fix(api): add graphql
    add graphql
    
    #10

--- apps/backend/api/src/app/app.controller.ts
+++ apps/backend/api/src/app/app.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {
+  }
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return {message: 'Welcome to backend/api'};
   }
 }
