56b7330b25b0619f74612f22d4dffeecb8edb687
dety0819 <luckystudydanit@gmail.com>
2021-07-10 20:12:53 +0300

    feat(root-store): add root-store lib [WiP]
    * add lib frontend-client/data-access/root-store
    * add root-state.interface
    * add router-url-state.interface
    
    #22

--- apps/backend/api/src/migrations/1625698873164-User.ts
+++ apps/backend/api/src/migrations/1625698873164-User.ts
@@ -12,16 +12,11 @@ export class User1625698873164 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       INSERT INTO users (username, email, password)
-      VALUES ('admin', 'admin@example.com', '$2a$10$d6wR5W1xIiTsQPG4aTuqIOkct0Dpr8TsciVS39YP3NSOwwy1wyPsy')
+      VALUES ('root', 'root@example.com', '$2a$10$d6wR5W1xIiTsQPG4aTuqIOkct0Dpr8TsciVS39YP3NSOwwy1wyPsy')
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-      DELETE
-      FROM users
-      WHERE username = 'admin
-      '
-    `);
+    await queryRunner.query(` DELETE FROM users WHERE username = 'admin'`);
   }
 }
